2ee3e87d4a9f95358e3f6aff8ec11a005b1c5f07
marigir <mariongirew@gmail.com>
2020-10-02 23:14:11 +0200

    Italienische Uebersetzung
    Uebersetzte Seiten, Language Navigation und ein paar generelle Korrekturen.

--- js/custom.js
+++ js/custom.js
@@ -1,38 +1,15 @@
-// Change Navbar on scroll
-
-$(window).scroll(function(){
-    if ($(this).scrollTop() > 35) {
-       $('#Navigation').removeClass('hide');
-       $('#Navigation').addClass('show');
-    } else {
-       $('#Navigation').removeClass('show');
-       $('#Navigation').addClass('hide');
-    }
-});
-
-$(window).scroll(function(){
-    if ($(this).scrollTop() > 35) {
-       $('#bannertext').removeClass('show');
-       $('#bannertext').addClass('hide');
-    } else {
-       $('#bannertext').removeClass('hide');
-       $('#bannertext').addClass('show');
-    }
-});
-
-
 // gallery
 $(function() {
-var selectedClass = "";
-$(".filter").click(function(){
-selectedClass = $(this).attr("data-rel");
-$("#gallery").fadeTo(100, 0.1);
-$("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
-setTimeout(function() {
-$("."+selectedClass).fadeIn().addClass('animation');
-$("#gallery").fadeTo(300, 1);
-}, 300);
-});
+  var selectedClass = "";
+  $(".filter").click(function(){
+    selectedClass = $(this).attr("data-rel");
+    $("#gallery").fadeTo(100, 0.1);
+    $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+    setTimeout(function() {
+      $("."+selectedClass).fadeIn().addClass('animation');
+      $("#gallery").fadeTo(300, 1);
+    }, 300);
+  });
 });
 
 //ekko-Lightbox
